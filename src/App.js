ef0da11a380ecce35ef0fedea1ba6d8b62526509
Erfan Moghadasi <erfan.moqadasii@gmail.com>
2022-06-13 17:53:44 +0430

    Done !

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Link } from "react-router-dom";
+import styled from "styled-components";
 
 //components
 import Category from "./components/Category";
 import Pages from "./pages/Pages";
 import Search from "./components/Search";
 
+import {GiKnifeFork} from 'react-icons/gi'
+
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+      <Nav>
+        <GiKnifeFork />
+        <Logo to={'/'}>delicious</Logo>
+      </Nav>
         <Search />
         <Category />
         <Pages />
@@ -16,5 +23,21 @@ function App() {
     </div>
   );
 }
+
+const Logo = styled(Link)`
+  text-decoration: none;
+  font-size: 1.5rem;
+  font-weight: 400;
+`
+const Nav = styled.div`
+  padding: 4rem 0;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  svg{
+    font-size: 2rem;
+  }
+`
+
 
 export default App;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,22 @@
-import React from 'react'
+import React from "react";
+import { motion } from "framer-motion";
 
 //components
-import Veggie from '../components/Veggie'
-import Popular from '../components/Popular'
+import Veggie from "../components/Veggie";
+import Popular from "../components/Popular";
 
 function Home() {
   return (
-    <div>
-        <Veggie />
-        <Popular />
-    </div>
-  )
+    <motion.div
+      animate={{ opacity: 1 }}
+      initial={{ opacity: 0 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 0.5 }}
+    >
+      <Veggie />
+      <Popular />
+    </motion.div>
+  );
 }
 
-export default Home
+export default Home;
